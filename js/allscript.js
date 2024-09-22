2645fad833ab238dc03bc04c89b098c4c0d94e8c
Haochuan Zhou <mrzhouhc@gmail.com>
2024-09-22 19:21:43 +1000

    for pull

--- js/allscript.js
+++ js/allscript.js
@@ -1,45 +1,40 @@
-let navbar = document.querySelector('.header .navbar');
+let navbar = document.querySelector(".header .navbar");
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.add('active');
-}
-
-document.querySelector('#nav-close').onclick = () =>{
-    navbar.classList.remove('active');
-}
-
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.add('active');
-}
-
-document.querySelector('#close-search').onclick = () =>{
-    searchForm.classList.remove('active');
-}
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.add("active");
 };
 
-window.onload = () =>{
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
+document.querySelector("#nav-close").onclick = () => {
+  navbar.classList.remove("active");
 };
 
+let searchForm = document.querySelector(".search-form");
 
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.add("active");
+};
 
+document.querySelector("#close-search").onclick = () => {
+  searchForm.classList.remove("active");
+};
 
+window.onscroll = () => {
+  navbar.classList.remove("active");
 
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
+
+window.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
 
 // -------------------------------------------to top scroller------------------------------------
 
@@ -51,27 +46,4 @@ window.addEventListener("scroll", () => {
   } else {
     toTop.classList.remove("active");
   }
-})
-
-
-
-
-
-
-// --------------------------------------------product main page js----------------------------------
-
-let tabs = document.querySelectorAll('.tabs__toggle'),
-    contents = document.querySelectorAll('.tabs__content');
-
-tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        contents.forEach((content) => {
-            content.classList.remove('is-active');
-        });
-        tabs.forEach((tab) => {
-            tab.classList.remove('is-active');
-        });
-        contents[index].classList.add('is-active');
-        tabs[index].classList.add('is-active');
-    });
 });
